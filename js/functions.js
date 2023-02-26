98ab33d8bc859f69ae3c29a5bfa31f4630021b6c
Ekaterina <catherinenekrylova@yandex.ry>
2023-02-26 09:59:07 +0200

    ESLint fix

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,21 @@
-let haveLength = (string, count) => string.length < count;
+const haveLength = (string, count) => string.length < count;
 
-let isPalindrome = (string) => {
+const isPalindrome = (string) => {
   string = string.toLowerCase().replaceAll(' ', '');
-  for (let i = 0; i<=string.length/2; i++) {
-    if (string[i] !== string[string.length-i-1]) {
+  for (let i = 0; i <= string.length / 2; i++) {
+    if (string[i] !== string[string.length - i - 1]) {
       return false;
     }
   }
   return true;
-}
+};
 
-let findNumber = (string) => Number(String(string).match(/\d/g)?.join(''));
+const findNumber = (string) => Number(String(string).match(/\d/g)?.join(''));
 
 function padStart (string, minLength, spacer) {
-while (string.length < minLength) {
-  const needToAdd=minLength-string.length; //она мне нужна для понимания кода, не трогай
-  string = (needToAdd < spacer.length ? spacer.slice(0, needToAdd) : spacer) + string;
-}
-return string;
+  while (string.length < minLength) {
+    const needToAdd = minLength - string.length; //она мне нужна для понимания кода, не трогай
+    string = (needToAdd < spacer.length ? spacer.slice(0, needToAdd) : spacer) + string;
+  }
+  return string;
 }
